d8f3913a765fba43359a35d2492c3c452dfd549d
erik.e.anrique <erik.anrique@wolox.com.ar>
2021-10-19 16:31:28 -0300

    added background colors

--- ComposeSimpleApp/app/src/main/java/com/example/myapplication/ItemListFragment.kt
+++ ComposeSimpleApp/app/src/main/java/com/example/myapplication/ItemListFragment.kt
@@ -1,14 +1,20 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class ItemListFragment : BaseComposableFragment() {
 
     @Composable
     override fun MainComposableLayout() {
-        Text(text = "LIST")
+        Surface(color = Color.Red, modifier = Modifier.fillMaxSize()) {
+            Text(text = "LIST")
+        }
     }
 
     @Preview(showBackground = true)
--- ComposeSimpleApp/app/src/main/java/com/example/myapplication/NewItemFragment.kt
+++ ComposeSimpleApp/app/src/main/java/com/example/myapplication/NewItemFragment.kt
@@ -1,14 +1,20 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class NewItemFragment : BaseComposableFragment() {
 
     @Composable
     override fun MainComposableLayout() {
-        Text(text = "NEW")
+        Surface(color = Color.Cyan, modifier = Modifier.fillMaxSize()) {
+            Text(text = "NEW")
+        }
     }
 
     @Preview(showBackground = true)
